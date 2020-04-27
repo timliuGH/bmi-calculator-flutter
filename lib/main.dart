99043c5e69d726d86290718becd392af83f54fbd
Timothy Liu <timsgithub@gmail.com>
2020-04-27 07:45:17 -0700

    Change ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,16 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        accentColor: Colors.purple,
+        textTheme: TextTheme(
+          body1: TextStyle(
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+      ),
       home: InputPage(),
     );
   }
