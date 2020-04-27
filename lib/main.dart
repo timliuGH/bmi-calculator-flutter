74889fe713468ccaf0ef287bb8c80905366ff27b
Timothy Liu <timsgithub@gmail.com>
2020-04-27 07:47:02 -0700

    Use copyWith on ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-        accentColor: Colors.purple,
-        textTheme: TextTheme(
-          body1: TextStyle(
-            color: Color(0xFFFFFFFF),
-          ),
-        ),
       ),
       home: InputPage(),
     );
