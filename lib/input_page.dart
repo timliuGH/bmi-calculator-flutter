ec9295f05de7091b40e573a7bddc6599203994cb
Timothy Liu <timsgithub@gmail.com>
2020-04-27 20:21:18 -0700

    Add child to ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -69,13 +69,15 @@ class _InputPageState extends State<InputPage> {
 
 class ReusableCard extends StatelessWidget {
   // Constructor with required named param
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
   // Properties of this class
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
