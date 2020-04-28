cc541fcae34f184f77b4728f432ed3c6debe69e8
Timothy Liu <timsgithub@gmail.com>
2020-04-27 18:03:33 -0700

    Add property and constructor to ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,25 +18,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(colour: Color(0xFF1D1E33)),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
               ],
             ),
@@ -48,12 +48,17 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
+  // Constructor with required named param
+  ReusableCard({@required this.colour});
+  // Properties of this class
+  final Color colour;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
