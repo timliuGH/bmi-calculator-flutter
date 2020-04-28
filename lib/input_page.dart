3bbfec15c4e0cc3a38015b52150b53e236d011ae
Timothy Liu <timsgithub@gmail.com>
2020-04-27 18:05:32 -0700

    Add Container to bottom of screen

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -40,6 +42,12 @@ class _InputPageState extends State<InputPage> {
                 ),
               ],
             ),
+          ),
+          Container(
+            color: Color(0xFFEB1555),
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10.0),
+            height: bottomContainerHeight,
           ),
         ],
       ),
